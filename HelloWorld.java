54ec2830345b05501d6fc5b01e1d08db14cd2ff7
srevereault <sylvain@revereault.fr>
2016-03-11 10:06:34 +0100

    Strawberry fields

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
 	public static void main (String [] args)
 	{
-		System.out.println("Java is the best programming language ever... ever! Really! I mean it!");
+		System.out.println("Java is the best programming language ever... ever! Really! I mean it! Forever");
 	}
 }
