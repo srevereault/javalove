a7a65268ffedcc25ee6a1c4f1d653ec68ecc8859
Benoit Averty <benoit.averty@zenika.com>
2016-03-11 11:01:42 +0100

    The only true language

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld
-{
-	public static void main (String [] args)
-	{
-		System.out.println("Java is the best programming language ever... ever! Really! I mean it! Forever");
-	}
-}
